48af2905febaccfad5c6c26be4acb84497854d1f
kamakship18 <kamakshipandoh8@gmail.com>
2024-01-06 22:15:31 +0530

    completed

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 /* Iteration 4: Make the Canvas App Functional */
+const imgface = document.getElementById('face-plate');
+const buttonface = document.getElementById('b-face');
+buttonface.oninput = () => {
+  imgface.style.fill = buttonface.value;
+};
+
+const imghelmet = document.getElementById('helmet');
+const buttonhelmet = document.getElementById('b-helmet');
+buttonhelmet.oninput = () => {
+  imghelmet.style.fill = buttonhelmet.value;
+};
+
+const imgeye = document.getElementById('eyes');
+const buttoneye = document.getElementById('b-eyes');
+buttoneye.oninput = () => {
+  imgeye.style.fill = buttoneye.value;
+};
+
+const imgbg = document.getElementById('background');
+const buttonbg = document.getElementById('b-bg');
+buttonbg.oninput = () => {
+  imgbg.style.fill = buttonbg.value;
+};
